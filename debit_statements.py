5da7c8ebad2683f95bd92bd0ea431b832a023ff6
Fernando Barros Sabalete <fbarross@everis.com>
2022-05-04 18:27:59 -0300

    include pixOut in debit_statements.py

--- debit_statements.py
+++ debit_statements.py
@@ -6,7 +6,8 @@ import logging
 
 def update_debit_statements_data(nu: Nubank, table_path: str):
     logger = logging.getLogger(__name__)
-    statements = list(filter(lambda x: x["__typename"] == "DebitPurchaseEvent", nu.get_account_statements()))
+    statements = nu.get_account_statements()
+    statements = list(filter(lambda x: x["__typename"] in ["DebitPurchaseEvent", "PixTransferOutEvent"], statements))
     logger.info(f"Successful statements request, returned {len(statements)} statements")
     data = list(map(get_relevant_data, statements))
     full_path = (Path(table_path) / "statements/debit_statements.xlsx")
