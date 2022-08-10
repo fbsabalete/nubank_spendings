2d18937117bbce26aa7a2f838558f7eaf73d8b47
FERNANDO DE BARROS SABALETE <fernando.sabalete@bradesco.com.br>
2022-08-10 12:50:57 -0300

    Code cleanup

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import sys
 from pathlib import Path
 
 from pynubank import Nubank, MockHttpClient
-from balances import update_balance_data
 from debit_statements import update_debit_statements_data
 
 from auth import authenticate
@@ -13,24 +12,21 @@ from bills import update_bills_data
 logging.basicConfig(
     format='%(asctime)s [%(levelname)s] %(message)s',
     level=logging.INFO, handlers=[
-    logging.FileHandler((Path(__file__).resolve().parent / "debug.log"), mode="w"),
-    logging.StreamHandler()
-])
+        logging.FileHandler((Path(__file__).resolve().parent / "debug.log"), mode="w"),
+        logging.StreamHandler()
+    ])
 
 logger = logging.getLogger(__name__)
 
 
 def get_from_nubank(table_path: str, nu: Nubank = Nubank()):
-
     authenticate(nu)
 
     update_bills_data(nu, table_path)
     update_debit_statements_data(nu, table_path)
-    # update_balance_data(nu, table_path)
 
 
 def mock_nubank(table_path: str, nu):
-    nu = Nubank(MockHttpClient())
     # authenticate(nu)
     nu.authenticate_with_cert("1231", "password", "cert.p12")
     statements = list(filter(lambda x: x["__typename"] == "DebitPurchaseEvent", nu.get_account_statements()))
@@ -64,6 +60,7 @@ sys.excepthook = handle_exception
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-path", "--path", "-p", help="Path to store .xlsx. Parent folder bills/ will be created if needed")
+    parser.add_argument("-path", "--path", "-p",
+                        help="Path to store .xlsx. Parent folder will be created if needed")
     parser.add_argument("-m", "--mock", "-mock", action='store_true')
     main(parser.parse_args())
